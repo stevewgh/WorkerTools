be136dda9a218f798b83e130b6daefd907a16e9d
Adam Beck <adamb3189@gmail.com>
2020-04-22 15:04:26 +1000

    fix: pathing

--- windows.ltsc2019/scripts/update_path.cmd
+++ windows.ltsc2019/scripts/update_path.cmd
@@ -1,2 +1,2 @@
-setx /M path "%PATH%;C:\google-cloud-sdk\bin"
-setx /M path "%PATH%;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\dotnet"
+setx /M path "%PATH%;C:\google-cloud-sdk\bin;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\dotnet;C:\Program Files\PackageManagement\NuGet\Packages\Octopus.Client.8.4.2\lib\net452\Octopus.Client.dll"
+
--- windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
+++ windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
@@ -5,57 +5,58 @@ Write-Host 'Running tests with Pester v'+$($pesterModules[0].Version)
 $PowershellVersion = '7.0.0';
 
 Describe  'installed dependencies' {
-    It 'powershell $PowershellVersion is installed' {
+    It 'has powershell $PowershellVersion is installed' {
         Write-Host $PSVersionTable.PSVersion
         $PSVersionTable.PSVersion.ToString() | Should Contain $PowershellVersion
     }
 
-    # It 'Octopus.Client is installed ' {
-    #     [Reflection.AssemblyName]::GetAssemblyName("C:\Octopus.Client.dll").Version.ToString() | Should Contain '8.4.0.0'
-    # }
+    It 'has Octopus.Client is installed ' {
+      [Reflection.AssemblyName]::GetAssemblyName("C:\Program Files\PackageManagement\NuGet\Packages\
+      Octopus.Client.8.4.2\lib\net452\Octopus.Client.dll").Version.ToString() | Should Contain '8.4.2.0'
+    }
 
-    # It 'dotnet is installed' {
-    #     dotnet --version | Should Contain '3.1.201'
-    # }
+    It 'has dotnet is installed' {
+        dotnet --version | Should Contain '3.1.201'
+    }
 
 
-    # It 'java is installed' {
-    #     java --version | Should Contain 'x.y.z'
-    # }
+    It 'has java is installed' {
+        java --version | Should Contain '14+36-1461'
+    }
 
-    # It 'az is installed' {
-    #     az --version | Should Contain 'x.y.z'
-    # }
+    It 'has az is installed' {
+        az --version | Should Contain '2.4.0'
+    }
 
-    It 'aws is installed' {
+    It 'has aws is installed' {
       Get-AWSPowerShellVersion | Should Contain '4.0.5'
     }
 
-    # It 'node is installed' {
-    #     node --version | Should Contain 'x.y.z'
-    # }
+    It 'has node is installed' {
+        node --version | Should Contain '12.16.2'
+    }
 
-    It 'kubectl is installed' {
+    It 'has kubectl is installed' {
         kubectl version --client | Should Contain '1.18.0'
     }
 
-    It 'helm is installed' {
-        helm version | Should Contain 'v3.1.2'
+    It 'has helm is installed' {
+        helm version | Should Contain '3.1.2'
     }
 
-    It 'terraform is installed' {
+    It 'has terraform is installed' {
         terraform version | Should Contain 'x.y.z'
     }
 
-    It 'python is installed' {
+    It 'has python is installed' {
         python --version | Should Contain '3.8.2'
     }
 
-    # It 'gcloud is installed' {
-    #     gcloud --version | Should Contain 'x.y.z'
-    # }
+    It 'has gcloud is installed' {
+        gcloud --version | Should Contain '289.0.0'
+    }
 
-    # It 'octo is installed' {
-    #     octo --version | Should Contain 'x.y.z'
-    # }
+    It 'has octo is installed' {
+        octo --version | Should Contain '7.3.4'
+    }
 }
