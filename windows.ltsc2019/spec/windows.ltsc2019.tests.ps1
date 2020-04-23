64faab936572b55b30fcd22b91d585024a007846
Adam Beck <adamb3189@gmail.com>
2020-04-23 16:49:02 +1000

    feat: get tests passing

--- windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
+++ windows.ltsc2019/spec/windows.ltsc2019.tests.ps1
@@ -2,60 +2,58 @@ $pesterModules = @( Get-Module -Name "Pester" -ErrorAction "SilentlyContinue" );
 
 Write-Host 'Running tests with Pester v'+$($pesterModules[0].Version)
 
-$PowershellVersion = '7.0.0';
+$PowershellVersion = '5.1.17763';
 
 Describe  'installed dependencies' {
     It 'has powershell $PowershellVersion is installed' {
-        Write-Host $PSVersionTable.PSVersion
-        $PSVersionTable.PSVersion.ToString() | Should Contain $PowershellVersion
+        $PSVersionTable.PSVersion.ToString() | Should Match $PowershellVersion
     }
 
     It 'has Octopus.Client is installed ' {
-      [Reflection.AssemblyName]::GetAssemblyName("C:\Program Files\PackageManagement\NuGet\Packages\
-      Octopus.Client.8.4.2\lib\net452\Octopus.Client.dll").Version.ToString() | Should Contain '8.4.2.0'
+      [Reflection.AssemblyName]::GetAssemblyName("C:\Program Files\PackageManagement\NuGet\Packages\Octopus.Client.8.4.2\lib\net452\Octopus.Client.dll").Version.ToString() | Should Match '8.4.2.0'
     }
 
     It 'has dotnet is installed' {
-        dotnet --version | Should Contain '3.1.201'
+        dotnet --version | Should Match '3.1.201'
     }
 
     It 'has java is installed' {
-        java --version | Should Contain '14+36-1461'
+      java --version | Select-String -Pattern '14\+36\-1461' | Should BeLike "*14+36-1461*"
     }
 
     It 'has az is installed' {
-        az --version | Should Contain '2.4.0'
+      az --version | Select-String -Pattern '2.4.0' | Should Match '2.4.0'
     }
 
     It 'has aws is installed' {
-      Get-AWSPowerShellVersion | Should Contain '4.0.5'
+      Get-AWSPowerShellVersion | Should Match '4.0.5'
     }
 
     It 'has node is installed' {
-        node --version | Should Contain '12.16.2'
+        node --version | Should Match '12.16.2'
     }
 
     It 'has kubectl is installed' {
-        kubectl version --client | Should Contain '1.18.0'
+        kubectl version --client | Should Match '1.18.0'
     }
 
     It 'has helm is installed' {
-        helm version | Should Contain '3.1.2'
+        helm version | Should Match '3.1.2'
     }
 
     It 'has terraform is installed' {
-        terraform version | Should Contain 'x.y.z'
+        terraform version | Should Match '0.12.24'
     }
 
     It 'has python is installed' {
-        python --version | Should Contain '3.8.2'
+        python --version | Should Match '3.8.2'
     }
 
     It 'has gcloud is installed' {
-        gcloud --version | Should Contain '289.0.0'
+      gcloud --version | Select-String -Pattern "289.0.0" | Should BeLike "*289.0.0*"
     }
 
     It 'has octo is installed' {
-        octo --version | Should Contain '7.3.4'
+        octo --version | Should Match '7.3.4'
     }
 }
